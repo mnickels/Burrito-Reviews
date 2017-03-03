96b6fb489fd25e518877c1d371bacfc7605da1d5
Brian <brianjor@uw.edu>
2017-03-02 16:13:18 -0800

    User class

---
+++ src/User.java
@@ -0,0 +1,56 @@
+
+/**
+ * Represents a user with a userId, name, and userType.
+ * 
+ * @author Brian
+ */
+public class User {
+
+    private int userId;
+    private String name;
+    private UserType type;
+
+    public User(final int userId, final String name, final UserType type) {
+        setUserId(userId);
+        setName(name);
+        setType(type);
+    }
+    
+    @Override
+    public String toString() {
+        return "User [name=" + name + ", type=" + type + "]";
+    }
+
+    private void setType(UserType type) {
+        if (type == null){
+            throw new IllegalArgumentException("Please enter a valid userType");
+        }
+        this.type = type;
+    }
+
+    public UserType getType() {
+        return type;
+    }
+
+    private void setName(String name) {
+        if (name == null || name.length() == 0) {
+            throw new IllegalArgumentException("Please enter a valid name");
+        }
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setUserId(int userId) {
+        if (userId < 1) {
+            throw new IllegalArgumentException("Please enter a valid userId");
+        }
+        this.userId = userId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+}
